2553f8f9b7d7870ba3864857b5c07228ba73c25b
Janne Blomqvist <janne.blomqvist@aalto.fi>
2017-05-09 13:54:39 +0300

    Update copyright year

--- src/ibtopotool.py
+++ src/ibtopotool.py
@@ -5,7 +5,7 @@
 
 """
     ibtopotool, a tool to do things with Infiniband topology.
-    Copyright (C) 2013-2016  Janne Blomqvist
+    Copyright (C) 2013-2017  Janne Blomqvist
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
