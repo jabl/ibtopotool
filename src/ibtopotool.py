b21fb00bd4f9dd564ba000e324e3d24f53d5f202
Janne Blomqvist <blomqvist.janne@gmail.com>
2020-05-26 10:35:34 +0300

    Switch to MPL 2.0 license

--- src/ibtopotool.py
+++ src/ibtopotool.py
@@ -4,22 +4,12 @@
 # vim: set fileencoding=utf-8
 
 """
-    ibtopotool, a tool to do things with Infiniband topology.
-    Copyright (C) 2013-2018  Janne Blomqvist
+ibtopotool, a tool to do things with Infiniband topology.
+Copyright (C) 2013-2020  Janne Blomqvist
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 
 import networkx as nx
